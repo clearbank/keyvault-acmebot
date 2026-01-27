c3b8f8f2764031839c0c655dc7946aaeb4dc3877
Jamie.Peacock <jamie.peacock@clear.bank>
2026-01-27 12:09:21 +0000

    Correct reference to options

--- KeyVault.Acmebot/Functions/SharedActivity.cs
+++ KeyVault.Acmebot/Functions/SharedActivity.cs
@@ -144,12 +144,12 @@ public class SharedActivity : ISharedActivity
     {
         var acmeProtocolClient = await _acmeProtocolClientFactory.CreateClientAsync();
 
-        if (options.Value.PreferredProfile is null)
+        if (_options.Value.PreferredProfile is null)
         {
             return await acmeProtocolClient.CreateOrderAsync(dnsNames);
         }
 
-        return await acmeProtocolClient.CreateOrderAsync(dnsNames, preferredProfile: options.Value.PreferredProfile);
+        return await acmeProtocolClient.CreateOrderAsync(dnsNames, preferredProfile: _options.Value.PreferredProfile);
     }
 
     [FunctionName(nameof(Dns01Precondition))]
